ae05793109ae54e114540e0740e8b3ec82e71307
mjoshi6 <mjoshi6@student.gsu.edu>
2024-04-05 18:15:10 -0400

    Music app structure , added application files for similarity search

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ Recommender platform for Music you like
 4) Setup python environment (new_requirement.txt)
 
 #### Information about files
+
+#### ETL
+1) loadData.py -> Loads data from local to AWS s3
+2) retrieveData -> Retrieves data from AWS s3
+3) mp3_to_vector -> create Embeddings
+4) create_input_for_datastax -> json for datastax
+
+#### Application 
+1) app.py - Entry point for user similarity search
+2) 
--- config.py
+++ config.py
@@ -1,20 +1,8 @@
+## ALL THE CONFIGURATIONS GO HERE
+
 import os
 from sqlalchemy import *
-
-
-'''
-#parameters for arctic databases
-db_username = 'svc-rspm'
-db_password = 'ADENOID-tray-sailfish'
-#db_host = 'recommendation-3.c8scn1wkslmg.us-east-2.rds.amazonaws.com'
-#db_host = 'localhost'
-db_host = 'arcresdb.rs.gsu.edu'
-db_port = '3306'
-#db_name = 'rspm'
-#db_name = 'rspmdev'
-db_name = 'RSPM_book_stage'
-'''
-
+from astrapy.db import AstraDB
 
 
 #parameters for local database BOOK
@@ -32,59 +20,11 @@ db_name = 'rspm_book_prod_new'
 # Create the database URL in the format required by SQLAlchemy
 DB_URI = f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 
+def connect_AstraDB():
+    # Initialize the client
+    db = AstraDB(
+        token="AstraCS:cJRHkPPfbcQCaJFKxnFjJoUE:d0eb7d0643c1255dd583daac12a86710650813c6f97f407b42399746d77be7a4",
+        api_endpoint="https://be61fb62-4f40-45db-8cd0-14c061a597ff-us-east-1.apps.astra.datastax.com")
 
-#DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://nabeeltariq2:nabeeltariq2@nabeeltariq2.cpliemudyxqt.us-east-1.rds.amazonaws.com/nabeeltariq6?charset=utf8')
-
-
-
-
-
-
-# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:sesame@localhost/movietweet?charset=utf8')
-
-
-
-# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:sesame@localhost/movietweet_ratings?charset=utf8')
-
-
-
-# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:sesame@localhost/movielensupgrade?charset=utf8')
-
-
-
-
-
-# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://nabeeltariq3:nabeeltariq3@nabeeltariq3.cpliemudyxqt.us-east-1.rds.amazonaws.com/nabeeltariq3?charset=utf8')
-
-
-
-
-# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://nabeeltariq2:nabeeltariq2@nabeeltariq2.cpliemudyxqt.us-east-1.rds.amazonaws.com/nabeeltariq2?charset=utf8')
-
-
-
-
-
-
-
-
-
-
-# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://nabeeltariq2:nabeeltariq2@aa1taiyw3rzbwis.cpliemudyxqt.us-east-1.rds.amazonaws.com/aa1taiyw3rzbwis?charset=utf8')
-
-
-
-# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://flask:nabeeltariq2@flasktest.cpliemudyxqt.us-east-1.rds.amazonaws.com/flaskdb?charset=utf8')
-
-
-# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:sesame@localhost/movielens_meta?charset=utf8')
-
-# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:sesame@localhost/amazonmusicalins2?charset=utf8')
-
-
-
-
-# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://flask:nabeeltariq2@aa1bus1tdvnpq5m.cpliemudyxqt.us-east-1.rds.amazonaws.com/ebdb?charset=utf8')
-
-#new database
-# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://nabeeltariq2:nabeeltariq2@aa14nod91ksnt0i.cpliemudyxqt.us-east-1.rds.amazonaws.com/ebdb?charset=utf8')
+    print("Connected to Astra DB")
+    return db
