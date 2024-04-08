81d37b3ddc6c85d6be4a522f87634bb365e57fe6
bachhoang144 <bachkhoa144@gmail.com>
2024-04-08 13:14:53 -0400

    Create a json transformer

--- create_input_for_datastax.py
+++ create_input_for_datastax.py
@@ -1,3 +1,17 @@
-# Dang Create Json as required for Datastax
+import os
+import mp3_to_vector_transformation
 
-#End A json file - store to data folder
+def create_input(completed_df):
+    output_folder = 'data/'
+    file_name = f'{output_folder}/music_vector_metadata.json'  
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True) 
+    # Convert DataFrame to JSON and save each row as a separate file
+    '''for index, row in completed_df.iterrows():     
+        json_data = row.to_json()     
+    # You can customize the file name as needed'''
+    json_data = completed_df.to_json(orient='records')
+    with open(file_name, 'w') as f:
+        f.write(json_data)
+    #End A json file - store to data folder
+    
