828a9d27d37d1bfdb25f363139e35a57d28b4d91
bachhoang144 <bachkhoa144@gmail.com>
2024-04-10 14:46:41 -0400

    Update Mp3_to_vector for optimization and subfolder processing.

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ import pandas as pd
 with open('data/input_song_file', 'r') as file:
     # Read the contents of the file
     input_song_vector = file.read()
+#before feature 1
+# have an input song from user in mp3 format, stored in application
+# convert this song into a vector. To do this:
+# call a pipeline creation, reaturn p  (create_pipeline)
+# call embedded fucntion, pass the p (check this)
+
 
 #print(input_song_vector)
 similar_songs = feature1.search_similar(input_song_vector)
